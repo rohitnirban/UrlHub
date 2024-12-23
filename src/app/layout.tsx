f05d2de87d76eaa87f670aaee9164c6bc698bbc4
Rohit Yadav <developer.royad@gmail.com>
2024-12-23 23:48:26 +0530

    refactor: remove unused icons from metadata in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,6 @@ const inter = Inter({ subsets: ["latin"], display:"swap" });
 export const metadata: Metadata = {
   title: "UrlHub - The Best URL Shortner",
   description: "UrlHub is a powerful and easy-to-use URL shortener that helps you manage and track your links effortlessly. Simplify your URLs, share them with ease, and monitor their performance with our comprehensive analytics.",
-  icons: [
-    {
-      url: "/login.png",
-    },
-  ],
 };
 
 export default function RootLayout({
