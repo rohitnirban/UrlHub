42351962bca56c0555fac3d0eee1c949edf42c57
Rohit Yadav <developer.royad@gmail.com>
2025-01-10 22:54:01 +0530

    refactor: update Navbar component to wrap buttons in Link for improved accessibility

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -23,12 +23,16 @@ export function Navbar() {
           </Link>
         </div>
         <div className="flex items-center space-x-4">
-          <Button variant="outline">
-            <Link href={'/login'}>Login</Link>
-          </Button>
-          <Button>
-            <Link href={'/register'}>Register</Link>
-          </Button>
+          <Link href={'/login'}>
+            <Button variant="outline">
+              Login
+            </Button>
+          </Link>
+          <Link href={'/register'}>
+            <Button>
+              Register
+            </Button>
+          </Link>
         </div>
       </div>
     </nav>
